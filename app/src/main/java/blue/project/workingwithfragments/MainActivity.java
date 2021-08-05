49676758b034444af133f687fb2b27b98410c753
Shoaib Akhtar <85933802+ShoaibAkhtar-Blue@users.noreply.github.com>
2021-08-05 13:38:45 +0500

    Add support for fragment transaction

--- app/src/main/java/blue/project/workingwithfragments/MainActivity.java
+++ app/src/main/java/blue/project/workingwithfragments/MainActivity.java
@@ -3,6 +3,8 @@ package blue.project.workingwithfragments;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirstFragment firstFragment = new FirstFragment();
+        SecondFragment secondFragment = new SecondFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.myFragmentLayout, firstFragment).commit();
+
+        Button firstFragmentButton = findViewById(R.id.button_fragment1);
+        Button secondFragmentButton = findViewById(R.id.button_fragment2);
+
+        firstFragmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.myFragmentLayout, firstFragment).commit();
+            }
+        });
+
+        secondFragmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.myFragmentLayout, secondFragment).commit();
+            }
+        });
     }
 }
